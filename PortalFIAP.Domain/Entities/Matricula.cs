8c73f3579190fe4e17c90d0cd74fe040261eb8de
nikolas.brisola <nikolas.brisola@gmail.com>
2026-03-08 20:28:53 +0000

    :recycle: refactor: Garante opcionalidade para atributo bolsa

--- PortalFIAP.Domain/Entities/Matricula.cs
+++ PortalFIAP.Domain/Entities/Matricula.cs
@@ -6,10 +6,16 @@ public class Matricula : BaseEntity
 {
     public Aluno Aluno { get; set; }
     public Turma Turma { get; set; }
-    public Bolsa Bolsa { get; set; }
+    public Bolsa? Bolsa { get; set; } 
 
-    public Matricula(Aluno aluno, Turma turma, Bolsa bolsa)
+    public Matricula(Aluno aluno, Turma turma, Bolsa? bolsa)
     {
+        if (aluno is null)
+            throw new ArgumentNullException(nameof(aluno), "O aluno não pode ser nulo.");
+
+        if (turma is null)
+            throw new ArgumentNullException(nameof(turma), "A turma não pode ser nula.");
+
         Aluno = aluno;
         Turma = turma;
         Bolsa = bolsa;
